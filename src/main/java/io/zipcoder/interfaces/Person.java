162170fdc589aaf3f33d036a2be267c12d120268
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-27 17:07:56 -0400

    Create Learner interface

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,26 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private final long id;
+    private String name="DefaultName";
+
+
+
+    public Person(long passedID, String passedName){
+        id=passedID;
+        setName(passedName);
+    }
+
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String passedName){
+        name=passedName;
+    }
 }
