f8919f1f42c669ecbf186f51653d6011e07dfd73
gpatelos <greg.patselas@gmail.com>
2017-10-28 09:52:07 -0400

    part 6 peopl teste class added

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,4 +1,65 @@
 package io.zipcoder.interfaces;
 
+//Part 6.1 - Create People class
+//
+//Create a People class.
+//        The class should instantiate an ArrayList field of Person objects named personList.
+//        The class should define a method named add which adds a Person to the personList.
+//        The class should define a method named findById which makes use of a long id parameter to return a Person
+//        object with the respective id field.
+//        The class should define a method named remove which makes use of a Person person parameter to remove a
+//        respective Person object.
+//        The class should define a method named remove which makes use of a long id parameter to remove a Person
+//        object with the respective id field.
+//        The class should define a method named getCount which returns the size of personList.
+//        The class should define a method named getArray which returns an array representation of the personList field.
+//        The class should define a named removeAll which clears our personList field.
+
+import java.util.ArrayList;
+
 public class People {
+
+    private ArrayList<Person> personList = new ArrayList<Person>();
+
+    public void addPerson(Person person){
+
+        personList.add(person);
+    }
+
+    public Person findById(long id){
+
+          for(int i = 0; i < personList.size(); i++){
+              if(personList.get(i).getId() == id){
+                return personList.get(i);
+              }
+          }return null;
+
+    }
+
+    public void removePerson(Person person){
+        personList.remove(person);
+    }
+
+    public void removeById(long id){
+         for(int i = 0; i < personList.size(); i++) {
+             if (personList.get(i).getId() == id) {
+                 personList.remove(personList.get(i));
+             }
+         }
+    }
+
+    public int getCount(){
+            return personList.size();
+    }
+
+    public String getArray(ArrayList<Person> personList){
+
+        return personList.toArray().toString();
+
+    }
+
+    public void removeAll(People personList){
+        personList.removeAll(personList);
+    }
+
 }
--- src/test/java/io/zipcoder/interfaces/PeopleTest.java
+++ src/test/java/io/zipcoder/interfaces/PeopleTest.java
@@ -1,4 +1,71 @@
 package io.zipcoder.interfaces;
 
+//Part 6.0 - Test People
+//
+//        Create a TestPeople class.
+//        Create a testAdd method which ensures that our personList in our People class populated with respective
+//        Person objects following invocation of the add method.
+//        Create a testRemove method which ensures that the personList in a People object is depopulated with a
+//        respective Person object following the invocation of the remove method.
+//        Create a testFindById method which ensures that a respective Person object with a respective id field is
+//        returned upon invocation of the findById method on a respective People object.
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class PeopleTest {
+
+    @Test
+    public void addPersonTest(){
+
+        People people = new People();
+        Person expected = new Student(1235);
+
+        people.addPerson(expected);
+
+        Person actual = people.findById(1235);
+
+        Assert.assertEquals(expected,actual);
+
+
+    }
+
+    @Test
+    public void removePersonTest(){
+        People people = new People();
+        Person instructorToRemove = new Instructor(1237);
+
+        people.addPerson(instructorToRemove);
+        people.removePerson(instructorToRemove);
+
+        int expected = 0;
+        int actual = people.getCount();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void removeByIDTest(){
+        People people = new People();
+        people.addPerson(new Instructor(1236));
+        people.removeById(1236);
+
+        int expected = 0;
+        int actual = people.getCount();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void findById(){
+        People people = new People();
+        Person expected = new Student(1232);
+        people.addPerson(expected);
+
+        Person actual = people.findById(1232);
+
+        Assert.assertEquals(expected,actual);
+    }
 }
