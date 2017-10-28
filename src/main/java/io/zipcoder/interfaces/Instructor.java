284017a965b88920855aae3a1570ffd331f6d591
gpatelos <greg.patselas@gmail.com>
2017-10-27 22:37:32 -0400

    part 4.0 done

---
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -0,0 +1,30 @@
+package io.zipcoder.interfaces;
+
+//Part 5.1 - Create Instructor Class
+//
+//        Create an Instructor class such that:
+//        Instructor is a subclass of Person
+//        Instructor implements the Teacher interface
+//        Instructor should have a concrete implementation of the teach method which invokes the learn method on the
+//                specified
+//                Learner object.
+//        Instructor should have a concrete implementation of the lecture method which invokes the learn method on each
+//        of the elements in the specified array of Learner objects.
+//        numberOfHours should be evenly split amongst the learners.
+//        double numberOfHoursPerLearner = numberOfHours / learners.length;
+
+
+public class Instructor extends Person implements Teacher {
+
+    public void teach(Learner learner, double numberOfHours){
+        learner.learn(numberOfHours);
+    }
+
+    public void lecture(Learner[] learners, double numberOfHours){
+        double numberOfHoursPerLearner = numberOfHours/learners.length
+        for(Learner currentElement: learners) {
+            currentElement.learn(numberOfHoursPerLearner);
+        }
+    }
+
+}
---
+++ src/test/java/io/zipcoder/interfaces/InstructorTest.java
@@ -0,0 +1,17 @@
+package io.zipcoder.interfaces;
+
+//Part 5.0 - Test Instructor
+//
+//        Create a TestInstructor class.
+//        Create a testImplementation method that asserts that an Instructor is an instanceof a Teacher.
+//        Create a testInheritance method that asserts that a Instructor is an instanceof a Person.
+//        Create a testTeach method that ensures when an Instructor invokes the .teach method, a respective student's
+//        totalStudyTime instance variable is incremented.
+//        Create a testLecture method that ensures when an Instructor invokes the .teach method, a respective student's
+//        totalStudyTime instance variable is incremented by the specified numberOfHours.
+
+public class InstructorTest {
+
+    @Test
+
+}
