f48b9acbae350f96dc9d33d53b140903da645096
gpatelos <greg.patselas@gmail.com>
2017-10-28 00:06:08 -0400

    LLL

--- src/main/java/io/zipcoder/interfaces/Instructor.java
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -16,12 +16,17 @@ package io.zipcoder.interfaces;
 
 public class Instructor extends Person implements Teacher {
 
+    public Instructor(long id) {
+        super(id);
+    }
+
     public void teach(Learner learner, double numberOfHours){
         learner.learn(numberOfHours);
     }
 
     public void lecture(Learner[] learners, double numberOfHours){
-        double numberOfHoursPerLearner = numberOfHours/learners.length
+        double numberOfHoursPerLearner = numberOfHours/learners.length;
+
         for(Learner currentElement: learners) {
             currentElement.learn(numberOfHoursPerLearner);
         }
--- src/test/java/io/zipcoder/interfaces/InstructorTest.java
+++ src/test/java/io/zipcoder/interfaces/InstructorTest.java
@@ -10,8 +10,54 @@ package io.zipcoder.interfaces;
 //        Create a testLecture method that ensures when an Instructor invokes the .teach method, a respective student's
 //        totalStudyTime instance variable is incremented by the specified numberOfHours.
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InstructorTest {
 
     @Test
+    public void testImplementation(){
+        Instructor instructor = new Instructor(1234);
 
+        boolean actual = instructor instanceof Teacher;
+
+        Assert.assertTrue(actual);
+
+
+    }
+
+    @Test
+    public void testInheritance(){
+        Instructor instructor = new Instructor(4321);
+
+        boolean actual = instructor instanceof Person;
+    }
+
+    @Test
+    public void testTeach(){
+        Student student = new Student(9876);
+        Instructor instructor = new Instructor(4567);
+        double expected = 10.0;
+
+        instructor.teach(student,10.0);
+        double actual = student.getTotalStudyTime();
+
+        Assert.assertTrue(expected==actual);
+
+    }
+
+    @Test
+    public void testLecture(){
+       Student[] students = {new Student(123), new Student(456)};
+
+
+        Instructor instructor = new Instructor(4567);
+        double expected = 25.0;
+
+        instructor.lecture(students,50.0);
+        double actual = students[0].getTotalStudyTime();
+
+        Assert.assertTrue(expected==actual);
+
+    }
 }
