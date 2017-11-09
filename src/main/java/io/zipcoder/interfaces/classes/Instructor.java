f79110b7b2ec63739e9e9451ad20cd6fe21abcaa
Zachary <zstimmel@gmail.com>
2017-11-09 07:49:00 -0500

    upto8.0

--- src/main/java/io/zipcoder/interfaces/classes/Instructor.java
+++ src/main/java/io/zipcoder/interfaces/classes/Instructor.java
@@ -6,7 +6,7 @@ import io.zipcoder.interfaces.interfaces.Teacher;
 public class Instructor extends Person implements Teacher {
 
 
-    public Instructor(long id){
+    public Instructor(long id) {
         super(id);
     }
 
@@ -15,8 +15,10 @@ public class Instructor extends Person implements Teacher {
     }
 
     public void lecture(Learner[] learners, double numberOfHours) {
-        double numberOfHoursPerLearner = numberOfHours/learners.length;
-        for(int i = 0;i < learners.length;i++){
+
+        double numberOfHoursPerLearner = numberOfHours / learners.length;
+
+        for (int i = 0; i < learners.length; i++) {
             learners[i].learn(numberOfHoursPerLearner);
         }
 
--- src/test/java/io/zipcoder/interfaces/classes/StudentsTest.java
+++ src/test/java/io/zipcoder/interfaces/classes/StudentsTest.java
@@ -1,5 +1,6 @@
 package io.zipcoder.interfaces.classes;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,11 +8,16 @@ import static org.junit.Assert.*;
 public class StudentsTest {
 
     @Test
-    public void getINSTANCE() {
+    public void TestStudents() {
 
         Students test = Students.getINSTANCE();
+        String expected = "Zach Stimmel";
 
-        
+        Student teststudent = (Student)test.findById(25);
+        String actual = teststudent.getName();
+
+        Assert.assertEquals(expected,actual);
+
 
 
 
