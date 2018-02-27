f96368dad0cd5e7e334c4061e62338e74963bbe6
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-27 15:27:15 -0500

    Parts 1 and 2

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,23 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    final long id;
+    String name;
+
+    public Person(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
