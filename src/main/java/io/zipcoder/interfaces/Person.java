499d6cd27841083c61f054cce9e35a2fcc016e5a
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-02-27 17:35:59 -0500

    Part 1 and 2 completed

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,23 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private final long id;
+    private String name;
+
+
+    public Person(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
