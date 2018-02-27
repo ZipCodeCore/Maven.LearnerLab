bf72961ef09051e8b111440805b04d739500e8a8
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-27 17:53:26 -0500

    Singleton started

---
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -0,0 +1,13 @@
+package io.zipcoder.interfaces;
+
+public class Students extends People{
+
+    private static final Students INSTANCE = new Students();
+
+    private Students(){
+
+    }
+
+
+
+}
---
+++ src/test/java/io/zipcoder/interfaces/TestStudents.java
@@ -0,0 +1,7 @@
+package io.zipcoder.interfaces;
+
+public class TestStudents {
+
+
+
+}
