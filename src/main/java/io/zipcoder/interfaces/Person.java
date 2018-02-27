6a29cce4999f9389e5072fe206845a131e885c55
Patrick Glavin <patjglavin@gmail.com>
2018-02-27 16:05:03 -0500

    set pom and person compiler

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    public final long id;
+    protected String name;
 
+    public Person(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,30 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestPerson {
+
+    Person tester;
+
+    @Before
+    public void setup(){
+        tester = new Person("Tester", 1);
+    }
+
+    @Test
+    public void constructorNameTest(){
+        String expected = "Tester";
+        String actual = tester.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorIDTest(){
+        long expected = 1;
+        long actual = tester.getId();
+        Assert.assertEquals(expected, actual);
+    }
 
 }
