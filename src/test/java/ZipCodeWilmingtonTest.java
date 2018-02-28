68b93aeceed51c99cb4fbe5016ddc2632e5efb5b
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-02-28 12:01:44 -0500

    fixed test for 9

--- src/test/java/ZipCodeWilmingtonTest.java
+++ src/test/java/ZipCodeWilmingtonTest.java
@@ -5,20 +5,24 @@ public class ZipCodeWilmingtonTest {
 
     @Test
     public void testHostLectureId(){
-    ZipCodeWilmington.getInstance().hostLecture(0, 20);
-    double expected = 2;
-    Student student = (Student)Students.getInstance().getArray()[0];
-    double actual = student.getTotalStudyTime();
+        Student student = (Student)Students.getInstance().getArray()[0];
+        double before = 0;
+        ZipCodeWilmington.getInstance().hostLecture(0, 20);
+        double after= student.getTotalStudyTime();
+        double expected = after - before;
+        double actual = after;
         Assert.assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void testHostLectureTeacher(){
-    Instructor Tariq = (Instructor) Instructors.getInstance().findById(0);
-    ZipCodeWilmington.getInstance().hostLecture(Tariq, 10);
-    double expected = 1;
-    Student student = (Student) Students.getInstance().getArray()[1];
-    double actual = student.getTotalStudyTime();
-    Assert.assertEquals(expected, actual, 0.01);
+        Student student = (Student) Students.getInstance().getArray()[0];
+        double before = 0;
+        Instructor tariq = (Instructor) Instructors.getInstance().findById(0);
+        ZipCodeWilmington.getInstance().hostLecture(tariq, 10);
+        double after = student.getTotalStudyTime();
+        double expected = after - before;
+        double actual = after;
+        Assert.assertEquals(expected, actual, 0.01);
     }
 }
