9b282d429d8ef94fb689a5ab046959d26f4af336
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-28 11:09:14 -0500

    ZCW singleton done ignore last commit

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -2,16 +2,31 @@ package io.zipcoder.interfaces;
 
 public class ZipCodeWilmington {
 
+    private static final ZipCodeWilmington INSTANCE = new ZipCodeWilmington();
+
     private Students students = Students.getInstance();
     private Instructors instructors = Instructors.getInstance();
 
 
     public void hostLecture(Teacher teacher, double numberOfHours) {
-        teacher.lecture(students, numberOfHours);
+        teacher.lecture(convertStudentsToLearnerArray(students), numberOfHours);
     }
 
     public void hostLecture(long id, double numberOfHours) {
+        Teacher teacher = (Teacher) Instructors.getInstance().findById(id);
+        teacher.lecture(convertStudentsToLearnerArray(students), numberOfHours);
+    }
 
+    public Learner[] convertStudentsToLearnerArray(Students originalStudents) {
+        Learner[] learnerArray = new Learner[originalStudents.getPersonList().size()];
+        for (int i = 0; i < learnerArray.length; i++) {
+            learnerArray[i] = (Learner) originalStudents.getPersonList().get(i);
+        }
+        return learnerArray;
+    }
+
+    public static ZipCodeWilmington getInstance(){
+        return INSTANCE;
     }
 
 }
--- src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
+++ src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
@@ -1,12 +1,17 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestZipCodeWilmington {
 
     @Test
     public void testHostLecture() {
-
+        ZipCodeWilmington.getInstance().hostLecture(9999, 10.0);
+        Student s = (Student) Students.getInstance().getPersonList().get(0);
+        double expected = 2.0;
+        double actual = s.getTotalStudyTime();
+        Assert.assertEquals(expected, actual, 0);
     }
 
 }
