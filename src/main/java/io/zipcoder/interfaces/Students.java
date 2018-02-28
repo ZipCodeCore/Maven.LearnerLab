6e6a1ac465cccadabd73622112d539a7e8615d38
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-28 07:54:40 -0500

    Students singleton complete

--- src/main/java/io/zipcoder/interfaces/Students.java
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -1,13 +1,30 @@
 package io.zipcoder.interfaces;
 
-public class Students extends People{
+public final class Students extends People {
 
     private static final Students INSTANCE = new Students();
 
     private Students(){
-
+        Student Mitch = new Student(1001);
+        Student Brian = new Student(1111);
+        Student Eric = new Student(2222);
+        Student Pete = new Student(3333);
+        Student Gio = new Student(4444);
+        getPersonList().add(Mitch);
+        getPersonList().add(Brian);
+        getPersonList().add(Eric);
+        getPersonList().add(Pete);
+        getPersonList().add(Gio);
     }
 
+    public static Students getInstance(){
+        return INSTANCE;
+    }
 
+    /*  Might not need this method
+    public static Students[] getStudentsArray() {
+        return (Students[]) getInstance().getPersonList().toArray();
+    }
+    */
 
 }
