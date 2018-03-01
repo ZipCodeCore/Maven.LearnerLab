8294d119b3817cdde6369fc9fcc08475ed35d6d2
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-03-01 13:07:57 -0500

    Fixed problem in 9.0 was not instanciating students

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -2,20 +2,21 @@ package io.zipcoder.interfaces;
 
 public final class ZipCodeWilmington extends People {
     private static ZipCodeWilmington instance = null;
-    private Students students;
+    private Students students = Students.getInstance();
     private Instructors instructors;
 
-    private ZipCodeWilmington(){
+    private ZipCodeWilmington() {
 
     }
 
     public void hostLectures(Teacher teacher, double numberOfHours) {
-        teacher.lecture(students.getStudentArray(), numberOfHours);
+        Student[] studentArray = students.getStudentArray();
+        teacher.lecture(studentArray, numberOfHours);
     }
 
     public void hostLectures(long id, double numberOfHours) {
-       Instructor name =  (Instructor) instructors.findById(id);
-       hostLectures(name, numberOfHours);
+        Instructor name = (Instructor) instructors.findById(id);
+        hostLectures(name, numberOfHours);
     }
 
 
