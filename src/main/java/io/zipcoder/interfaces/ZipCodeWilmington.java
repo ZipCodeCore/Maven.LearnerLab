02c0b849c850aa0f246b0a8b9e8dff743924dc93
CWinarski <Winarski70@yahoo.com>
2018-02-28 23:10:51 -0500

    Almost finished part 9

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -1,4 +1,25 @@
 package io.zipcoder.interfaces;
 
-public class ZipCodeWilmington {
+import Interfaces.Learner;
+import Interfaces.Teacher;
+
+public final class ZipCodeWilmington {
+
+    private static final ZipCodeWilmington ZIPCODEWILMINGTON = new ZipCodeWilmington();
+    private Students students = Students.getInstance();
+    private Instructors instructors = Instructors.getInstance();
+
+
+    public void hostLecture(Teacher teacher, double numberOfHours){
+        teacher.lecture((Learner[]) students.getArray(), numberOfHours);
+    }
+
+    public void hostLecture(long id, double numberOfHours){
+        Instructor instructor1 = (Instructor) instructors.findById(id);
+        instructor1.lecture((Learner[]) students.getArray(), numberOfHours);
+    }
+
+    public static ZipCodeWilmington getInstance(){
+        return ZIPCODEWILMINGTON;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/ZipCodeWilmingtonTest.java
+++ src/test/java/io/zipcoder/interfaces/ZipCodeWilmingtonTest.java
@@ -1,4 +1,31 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ZipCodeWilmingtonTest {
+    @Test
+    public void hostLectureTest(){
+        //Given
+        ZipCodeWilmington.getInstance().hostLecture(2000,150);
+        Student student = (Student) Students.getInstance().personList.get(0);
+        double expected = 30;
+        //When
+        double actual = student.getTotalStudyTime();
+        Assert.assertEquals(expected, actual,0);
+
+    }
+
+    @Test
+    public void hostLecture2Test(){
+        //Given
+        Instructor instructor =(Instructor) Instructors.getInstance().personList.get(1);
+        ZipCodeWilmington.getInstance().hostLecture(instructor,150);
+        Student student = (Student) Students.getInstance().personList.get(0);
+        double expected = 30;
+        //When
+        double actual = student.getTotalStudyTime();
+        Assert.assertEquals(expected, actual,0);
+
+    }
 }
