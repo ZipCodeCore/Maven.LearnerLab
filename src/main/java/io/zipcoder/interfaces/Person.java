3197c5c61c7d3bb253d64b0fd6e4eb1f82224db3
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-03-01 14:30:14 -0500

    finished part 1

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,24 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private final long ID;
+    private String name;
+
+    public Person(long ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
