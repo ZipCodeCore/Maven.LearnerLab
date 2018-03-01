0f881b2b1800f787ba634ac91c42785e7210511f
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-03-01 18:03:27 -0500

    starting enums

--- src/main/java/io/zipcoder/interfaces/Educator.java
+++ src/main/java/io/zipcoder/interfaces/Educator.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
-public class Educator {
+public enum Educator implements Teacher {
+    private double timeWorked;
+    private final long id;
+
+    KRIS(0),
+    TARIQ(1),
+    LEON(2);
+
+    
+    //constructor
+    Educator(long id) {
+        this.id =id;
+    }
+
+    public void teach(Learner learner, double numberOfHours) {
+    }
+
+    public void lecture(Learner[] learners, double numberOfHours) {
+    }
 
 }
