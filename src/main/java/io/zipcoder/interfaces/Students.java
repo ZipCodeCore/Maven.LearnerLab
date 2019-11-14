f4ecbd3171e21b5c9ccf50343315021fd7ec2069
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-14 18:10:52 -0500

    Add test for getInstance

--- src/main/java/io/zipcoder/interfaces/Students.java
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -1,4 +1,18 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Students {
+    private static final List<Student> INSTANCE = new ArrayList<>();
+
+    private Students(Student student){
+        // private constructor to avoid client applications to use constructor
+        INSTANCE.add(student);
+    }
+
+    public static List<Student> getInstance(){
+        return INSTANCE;
+    }
 }
---
+++ src/test/java/io/zipcoder/interfaces/StudentsTest.java
@@ -0,0 +1,27 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+
+public class StudentsTest {
+
+    @Test
+    public void getInstance() {
+        List<Student> testStudents = Students.getInstance();
+        Student student1 = new Student(32L,"Joe");
+        Student student2 = new Student(31L,"Jack");
+
+        testStudents.add(student1);
+        testStudents.add(student2);
+
+        for (Student eachS : testStudents){
+            System.out.println(eachS.getName() + " - " + eachS.getId());
+        }
+
+        Integer expected = 2;
+        Integer actual = testStudents.size();
+        Assert.assertEquals(expected,actual);
+    }
+}
