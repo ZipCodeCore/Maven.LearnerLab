18f49893304511dc8be789119af2684b727b3f9c
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-14 12:29:05 -0500

    Add logic to instructor

--- src/main/java/io/zipcoder/interfaces/Instructor.java
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -1,4 +1,28 @@
 package io.zipcoder.interfaces;
 
-public class Instructor {
+import interfaces.Learner;
+import interfaces.Teacher;
+
+public class Instructor extends Person implements Teacher {
+
+
+//-----------  constructor  ---------------------------
+    public Instructor(Long id, String name) {
+        super(id, name);
+    }
+
+
+//-----------  interfaces  ---------------------------
+
+    public void teach(Learner learner, double numberOfHours) {
+        learner.learn(numberOfHours);
+    }
+
+    public void lecture(Learner[] learners, double numberOfHours) {
+        double numberOfHoursPerLearner = numberOfHours / learners.length;
+
+        for(Learner eachLearner : learners){
+            eachLearner.learn(numberOfHoursPerLearner);
+        }
+    }
 }
