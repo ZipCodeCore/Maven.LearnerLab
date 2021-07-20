d661c242e5cb81621f071e01b030a4a0bf8b9133
Nick <nick@zipcodes-mbp-78.myfiosgateway.com>
2021-07-20 14:49:36 -0400

    Pre lecture

---
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -0,0 +1,14 @@
+package io.zipcoder.interfaces;
+
+public class Instructor extends Person implements Teacher {
+
+
+
+    public void teach(Learner learner, double numberOfHours) {
+       
+    }
+
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+}
--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,30 @@
 package io.zipcoder.interfaces;
 
-public class Person {
+import sun.util.resources.ga.LocaleNames_ga;
 
+public class Person {
+    final long id;
+    String name;
+
+    public Person () {
+        this.id = Long.MIN_VALUE;
+        this.name = "";
+    }
+
+    public Person (long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
