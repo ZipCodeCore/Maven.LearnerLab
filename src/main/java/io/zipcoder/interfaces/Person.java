8e59bb22f74805da835b352230e02aa93b08c445
sitarameez <sitarablossom@gmail.com>
2021-07-24 16:10:50 -0400

    part 2 done

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    final Long id;
+    String name;
 
+    public Person(Long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,46 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor() {
+        //given
+        String expectedName = "rameez";
+        Long id = 4l;
+        Person person = new Person(4l, "rameez");
+        //when
+        String actual = person.getName();
+        //then
+        Assert.assertEquals(expectedName, actual);
+    }
+
+    @Test
+    public void testConstructorForId() {
+        //given
+        String expectedName = "rameez";
+        Long expectedId = 4l;
+        Person person = new Person(4l, "rameez");
+        //when
+        Long actual = person.getId();
+        //then
+        Assert.assertEquals(expectedId, actual);
+    }
+
+    @Test
+    public void testSetName() {
+        //given
+
+        Long id = 4l;
+        Person person = new Person(4l, "rameez123");
+        String expectedName = "rameez";
+        //when
+        person.setName(expectedName);
+        String actual = person.getName();
+        //then
+        Assert.assertEquals(expectedName, actual);
+    }
 }
