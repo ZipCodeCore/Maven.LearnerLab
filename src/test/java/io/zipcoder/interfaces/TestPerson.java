4243e7c059c9ebfb7f7b7e43c63ab6cd33f8374c
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-25 12:02:13 -0400

    did till 2.0

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,33 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+    @Test
+    public void testConstructor(){
+        //given
+        Long expectedId=21l;
+        String expectedName="amilia";
+        //when
+        Person amilia=new Person(expectedId,expectedName);
+        Long actualId=amilia.getId();
+        String actuaName=amilia.getName();
+        //then
+        Assert.assertEquals(expectedId,actualId);
+        Assert.assertEquals(expectedName,actuaName);
+    }
+
+    @Test
+    public void testSetName(){
+        //given
+        Person person=new Person(23l,"georgia");
+        String expectedName="fabiana";
+        //when
+        person.setName(expectedName);
+        String actualName=person.getName();
+        //then
+        Assert.assertEquals(expectedName,actualName);
+    }
 
 }
